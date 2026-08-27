f70aacc54225121d00dfbe603d8742e2b6fb3b88
Pedro Lima <pedrolimake19@gmail.com>
2026-08-27 13:37:41 -0300

    Improve output messages and divisor reporting
    Updated print statements for better readability and added output for divisors.

--- 03_loops/numero_primo.py
+++ 03_loops/numero_primo.py
@@ -1,16 +1,24 @@
-n = int(input('digite um numero'))
+n = int(input('Digite um número: '))
 primo = True
 
+lista = []
+
 if n < 2:
-    print(f'o numero {n} não é primo')
+    print(f'O número {n} não é primo')
 
 else:
-    
     for divisor in range(2, n):
         if n % divisor == 0:
+            lista.append(divisor)
             primo = False
 
     if primo:
-        print(f'o numero {n} é primo')
+        print(f'O número {n} é primo')
     else:
-        print(f'o numero {n} não é primo')
+        print(f'O número {n} não é primo')
+        print('É divisível por 1')
+
+        for divisor in lista:
+            print(f'É divisível por {divisor}')
+
+        print(f'E é divisível por {n}')
